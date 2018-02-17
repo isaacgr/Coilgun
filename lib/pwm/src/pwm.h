19b6fe772cbb039794a6a9e461f830c5251c6aa7
isaacgr <isaacgrowell@gmail.com>
2018-02-17 00:12:51 -0500

    Use Timer0 instead of Timer1 Remove FREQUENCY variable from pwm_set Use OCR0A as TOP and compare to OCR0B Remove prescalar Fix comments

--- lib/pwm/src/pwm.h
+++ lib/pwm/src/pwm.h
@@ -8,7 +8,7 @@
 #define PWM_H
 #include "stdint.h"
 
-int pwm_set(uint16_t DUTY, uint16_t FREQUENCY); // set the duty cycle and frequency
+int pwm_set(uint16_t DUTY); // set the duty cycle
 int pwm_init(void); // initialize the pwm modue, return 0 on success
 
 #endif
