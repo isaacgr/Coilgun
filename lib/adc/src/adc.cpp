9dd0ecb5df26ac32ed37d5e924918b946d930d68
isaacgr <isaacgrowell@gmail.com>
2018-02-17 18:36:17 -0500

    Increase the ADC prescalar to 64 Disbale the digial input pin

--- lib/adc/src/adc.cpp
+++ lib/adc/src/adc.cpp
@@ -9,10 +9,11 @@
 int adc_init(void)
 {
   ADMUX = 0;  // use ADC0
+  DIDR0 |= (1<<ADC0D); // disable the digial inputs on the adc
   ADMUX |= (1<<REFS0);  // AVcc as the reference
   ADMUX |= (0<<ADLAR);  // left justified, 10bit resolution
 
-  ADCSRA |= (0<<ADPS2) | (1<<ADPS1) | (0<<ADPS0); // 4 prescaler
+  ADCSRA |= (1<<ADPS2) | (1<<ADPS1) | (0<<ADPS0); // 64 prescaler
   ADCSRA |= (1<<ADATE); // enable auto trigger
 
   ADCSRB = 0; // free running mode
