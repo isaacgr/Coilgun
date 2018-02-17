11cfd146d91992c975304d78ccb158d914e6ddde
isaacgr <isaacgrowell@gmail.com>
2018-02-17 00:11:38 -0500

    Lower ADC prescalar Fix comments

--- lib/adc/src/adc.cpp
+++ lib/adc/src/adc.cpp
@@ -8,11 +8,11 @@
 
 int adc_init(void)
 {
-  ADMUX = 0;  // use ADC0 and ADC1
+  ADMUX = 0;  // use ADC0
   ADMUX |= (1<<REFS0);  // AVcc as the reference
   ADMUX |= (0<<ADLAR);  // left justified, 10bit resolution
 
-  ADCSRA |= (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0); // 128 prescaler
+  ADCSRA |= (0<<ADPS2) | (1<<ADPS1) | (0<<ADPS0); // 4 prescaler
   ADCSRA |= (1<<ADATE); // enable auto trigger
 
   ADCSRB = 0; // free running mode
