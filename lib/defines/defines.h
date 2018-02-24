ea0a5350ae849fad1a89c122deaf571c109438c6
isaacgr <isaacgrowell@gmail.com>
2018-02-24 00:35:54 -0500

    Use a more accurate divider value Add temporary variables to definitions

--- lib/defines/defines.h
+++ lib/defines/defines.h
@@ -1,8 +1,13 @@
 #ifndef DEFINES_H
 #define DEFINES_H
+#include <avr/io.h>
 
 #define MAX_DUTY 255
 #define MIN_DUTY 255*0.01
-#define DIVIDER 6.0*5.0/1023.0
+#define DIVIDER 5.94*5.0/1023.0
+
+// temporary variables that needed to be global
+volatile uint8_t i = 0;
+volatile uint16_t sum = 0;
 
 #endif
