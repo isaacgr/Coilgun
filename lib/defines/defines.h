2ea1f8e929557cedaf86dde52fdf609ea96c5f55
isaacgr <isaacgrowell@gmail.com>
2018-02-28 22:12:45 -0500

    Add definitions for CURR_DIV and ACS_OFFSET

--- lib/defines/defines.h
+++ lib/defines/defines.h
@@ -4,7 +4,9 @@
 
 #define MAX_DUTY 255
 #define MIN_DUTY 255*0.01
-#define DIVIDER 5.94*5.0/1023.0
+#define VOLT_DIV 5.94*5.0/1023.0
+#define ACS_OFFSET 2.5  //acs712 voltage offset
+#define CURR_DIV ((5.0-ACS_OFFSET)/(1023.0*0.066)) // mV/A
 
 // temporary variables that needed to be global
 volatile uint8_t i = 0;
