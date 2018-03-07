437563650b7067ee46c3faef9f40e8f4fe58a2c5
isaacgr <isaacgrowell@gmail.com>
2018-03-07 07:55:09 -0500

    Add definitions for voltage and current array sizes

--- lib/defines/defines.h
+++ lib/defines/defines.h
@@ -10,12 +10,16 @@
 #define ACS_MIN 102
 #define ACS_MAX 930
 
-
 // temporary variables that needed to be global
 volatile uint8_t i = 0;
 volatile uint8_t j = 0;
 volatile uint16_t volt_sum = 0;
 volatile uint16_t curr_sum = 0;
+const int voltage_arr = 32;
+const int curr_arr = 16;
+
+
+// initialize VCC and variables that rely on it
 float VCC = 0;
 float ACS_OFFSET;
 float SENSITIVITY;
