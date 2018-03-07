4cc60fa6ed4c39b6391d646f18f61f3f8fc739bc
isaacgr <isaacgrowell@gmail.com>
2018-03-07 07:53:35 -0500

    Store and recall previous ADMUX value when calling readVcc

--- lib/adc/adc.cpp
+++ lib/adc/adc.cpp
@@ -29,21 +29,15 @@ int adc_init(void)
   return 0;
 }
 
-long readVcc()
+long readVcc(void)
 {
   // Read 1.1V reference against AVcc
   // set the reference to Vcc and the measurement to the internal 1.1V reference
-  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-  ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
-  ADMUX = _BV(MUX5) | _BV(MUX0);
-  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
-  ADMUX = _BV(MUX3) | _BV(MUX2);
-  #else
+  uint8_t tmp;
+  tmp = ADMUX;
+
   ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  #endif
-  _delay_ms(2);
-  //delay(2); // Wait for Vref to settle
+  _delay_ms(2); // wait for vref to settle
   ADCSRA |= _BV(ADSC); // Start conversion
   while (bit_is_set(ADCSRA,ADSC)); // measuring
 
@@ -53,5 +47,9 @@ long readVcc()
   long result = (high<<8) | low;
 
   result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+
+  ADMUX = tmp;  // reset ADMUX to value set by adc_init()
+  ADCSRA |= (1<<ADSC); // start conversion
+
   return result; // Vcc in millivolts
 }
