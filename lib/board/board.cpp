798ee40b45b8fe1c4bbe6627e57980cd15d4272a
isaacgr <isaacgrowell@gmail.com>
2018-03-17 18:46:23 -0400

    Add comments

--- lib/board/board.cpp
+++ lib/board/board.cpp
@@ -11,15 +11,10 @@ int board_init(void){
   DDRD &= ~(1<<DDD2);  // PORTB pin 5 as input
 
   PORTD |= (1 << PORTD2);    // turn On the Pull-up
-     // PD2 is now an input with pull-up enabled
-     
-  EICRA |= (1<<ISC01) | (1<<ISC00);
-  EIMSK |= (1<<INT0);
-  EIFR |= (1<<INTF0);
 
-  // PCICR |= (1<<PCIE0);  // enable interrupt for this pin group
-  // PCIFR |= (1<<PCIF0);  // enable interrupt flag
-  // PCMSK0 |= (1<<PCINT5);  // enable interrupt for this pin
+  EICRA |= (1<<ISC01) | (1<<ISC00); // trigger on rising edge
+  EIMSK |= (1<<INT0);  // enable interrupt for this pin
+  EIFR |= (1<<INTF0); // enable interrupt flag
 
   return 0;
 }
