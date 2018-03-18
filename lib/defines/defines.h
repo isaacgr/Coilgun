93e589c9dab5f292d4d00820171e0640ae7a0f6e
isaacgr <isaacgrowell@gmail.com>
2018-03-18 07:37:25 -0400

    Add comments for sections Set MIN_DUTY to 0 Include definitions for PID direction Include all variables from main() Remove volatile keyword from some variables

--- lib/defines/defines.h
+++ lib/defines/defines.h
@@ -4,21 +4,55 @@
 #include <avr/io.h>
 #include <adc.h>
 
+/*****************
+DEFINITIONS
+******************/
 #define MAX_DUTY 255
-#define MIN_DUTY 1
+#define MIN_DUTY 0
 #define VOLT_DIV 11*BIT_DIV
 #define ACS_MIN 102
 #define ACS_MAX 930
+#define DIRECT 0
+#define REVERSE 1
 
+/*****************
+TEMPORARY VARIABLES
+******************/
 // temporary variables that needed to be global
 volatile uint8_t i = 0;
 volatile uint8_t j = 0;
-volatile uint16_t volt_sum = 0;
-volatile uint16_t curr_sum = 0;
+uint16_t volt_sum = 0;
+uint16_t curr_sum = 0;
 const int voltage_arr = 32;
 const int curr_arr = 16;
 
+/*****************
+VOLATILE VARIABLES
+******************/
+volatile uint16_t OUTPUT_VOLTAGE_ARRAY[voltage_arr];
+volatile uint16_t ADC_VALUE;
+volatile uint8_t ADCLOW;
+volatile uint16_t CURRENT_RAW[curr_arr];
+volatile float CURRENT;
+volatile uint8_t TIMER_DELAY = 0;
+volatile uint8_t PAGE = 0;
+volatile uint16_t millisec = 0;
+volatile float OUTPUT_VOLTAGE;
+volatile float SETPOINT;
+volatile float VIN;
 
+/*****************
+PID VARIABLES
+******************/
+uint8_t DUTY;
+float kp, ki;
+double kd;
+double errSum, lastInput;
+unsigned long lastTime;
+
+/*****************
+VCC VARIABLES
+******************/
 // initialize VCC and variables that rely on it
 float VCC = 0;
 float ACS_OFFSET;
